af475d6182bf3ae6ba2171c6e7ba2f82a4200aa3
ivan <veloso_32@hotmail.com>
2025-02-08 20:36:17 +0100

    [fix]: se resuelven los errores para los test.

--- src/test/scala/VelESPro/MoleculeTest.scala
+++ src/test/scala/VelESPro/MoleculeTest.scala
@@ -14,7 +14,7 @@ class MoleculeTest extends AnyWordSpec with Matchers {
 
   import spark.implicits._
 
-  val in_mol1 = "/home/iveloso/IdeaProjects/Scala_VelESPro/src/test/resources/Input/input1.vel"
+  val in_mol1 = "src/test/resources/Input/input1.vel"
   val nombre = "water"
   val r_mol = new Molecule(in_mol1)
 
@@ -44,7 +44,7 @@ class MoleculeTest extends AnyWordSpec with Matchers {
     coord(0.0000000000000000, -0.14322574511573555, 0.0000000000000000)
   ).toDF()
   //Lista de valores del centro de masas
-  private val c_mass1 = List(BigDecimal("0E-15").setScale(15), BigDecimal("-0.000017311920851").setScale(15), BigDecimal("0E-15").setScale(15))
+  private val c_mass1 = List(BigDecimal("0E-13").setScale(13), BigDecimal("-0.0000173119209").setScale(13), BigDecimal("0E-13").setScale(13))
   //Convert the scala bigDecimal to java Bigdecimal that is the return value for spark
   private val c_mass1_j = c_mass1.map(_.bigDecimal)
 
@@ -54,28 +54,28 @@ class MoleculeTest extends AnyWordSpec with Matchers {
 
     }
     "obtain the atomic number of the atoms" in {
-      val c_ch = r_mol.c_at_f.collect().map(_.getAs[Int]("Num_at")).toList
+      val c_ch = r_mol.c_at_f.orderBy("AtomID").collect().map(_.getAs[Int]("Num_at")).toList
       c_ch shouldBe c_ch1
     }
     "obtain the mass of the atoms" in {
-      val masa = r_mol.c_at_f.collect().map(_.getAs[Double]("Mass")).toList
+      val masa = r_mol.c_at_f.orderBy("AtomID").collect().map(_.getAs[Double]("Mass")).toList
       masa shouldBe mass1
     }
     "obtain the center of mass" in {
-      val cast_cm = r_mol.mol_f.withColumn(Par.c_cm, col(Par.c_cm).cast("array<decimal(25,15)>"))
+      val cast_cm = r_mol.mol_f.withColumn(Par.c_cm, col(Par.c_cm).cast("array<decimal(25,13)>"))
       val c_mass = cast_cm.collect().map(_.getAs[Seq[BigDecimal]]("Center_mass")).apply(0).toList
       c_mass shouldBe c_mass1_j
     }
   }
 
-  val in_mol2 = "/home/iveloso/IdeaProjects/Scala_VelESPro/src/test/resources/Input/input2.vel"
+  val in_mol2 = "src/test/resources/Input/input2.vel"
   val nombre2 = "test_at"
   val r_mol1 = new Molecule(in_mol2)
 
   //Lista de valores del centro de masas
-  private val c_mass2 = List(BigDecimal("0.24216318999483818").setScale(15, RoundingMode.HALF_EVEN),
-    BigDecimal("-0.080676815136674446").setScale(15, RoundingMode.HALF_EVEN),
-    BigDecimal("-0.09521388579237054").setScale(15, RoundingMode.HALF_EVEN))
+  private val c_mass2 = List(BigDecimal("0.24216318999483818").setScale(13, RoundingMode.HALF_EVEN),
+    BigDecimal("-0.080676815136674446").setScale(13, RoundingMode.HALF_EVEN),
+    BigDecimal("-0.09521388579237054").setScale(13, RoundingMode.HALF_EVEN))
   private val c_mass2_j = c_mass2.map(_.bigDecimal)
 
   "Molecule with two molecule" should {
@@ -89,7 +89,7 @@ class MoleculeTest extends AnyWordSpec with Matchers {
   it should {
     "obtain the center of mass" in {
       val name = r_mol1.mol_f.collect().map(_.getAs[String]("Name"))
-      val cast_cm = r_mol1.mol_f.withColumn(Par.c_cm, col(Par.c_cm).cast("array<decimal(25,15)>"))
+      val cast_cm = r_mol1.mol_f.withColumn(Par.c_cm, col(Par.c_cm).cast("array<decimal(25,13)>"))
       val c_mass = cast_cm.collect().map(_.getAs[Seq[Double]]("Center_mass").toList)
       val map_nm = name.zip(c_mass).toMap
       map_nm("water") shouldBe c_mass1_j
