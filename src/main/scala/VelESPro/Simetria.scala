f71d40e67c4019a825316043ecb1904be88c2a24
ivan <ivan.cluster32@gmail.com>
2025-02-09 19:44:12 +0100

    [feature]: Se añade el SEA y se introduce el nombre del input como una variable a pasar.

--- src/main/scala/VelESPro/Simetria.scala
+++ src/main/scala/VelESPro/Simetria.scala
@@ -45,6 +45,17 @@ class Simetria(mol: DataFrame) {
     m_inercia_f
   }
 
-  val n = mat_iner(mol)
+  // Matriz de inercia
+  val m_iner = mat_iner(mol)
+
+  // SEA
+  val dist_sort = mol.withColumn("Dist_at_s", array_sort(col("Dist_at")))
+  // Se pone el array decimal dependiendo de lo ajustado que queremos el resultado
+  val dist_sort1 = dist_sort.withColumn("Dist_at_s", col("Dist_at_s").cast("array<decimal(25,7)>"))
+  val SEA = dist_sort1.select("Dist_at_s","AtomID").groupBy("Dist_at_s").agg(collect_set("AtomID").as("C_AtomID")).orderBy("C_AtomID").withColumn("SEA_ID", monotonically_increasing_id)
+  val mol_SEA = dist_sort1.join(SEA,Seq("Dist_at_s"), "inner")
+  val mol_SEA1 = mol_SEA.drop("Dist_at_s","C_AtomID")
+
+  val mm = 1
 
 }
